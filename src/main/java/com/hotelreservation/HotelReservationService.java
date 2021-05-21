f847a2b2359708531b88d69baf2d489110ae4222
Batman2532 <charjan44@gmail.com>
2021-05-21 16:42:40 +0530

    Added: cheapest hotel by given date range

--- src/main/java/com/hotelreservation/HotelReservationService.java
+++ src/main/java/com/hotelreservation/HotelReservationService.java
@@ -1,11 +1,15 @@
 package com.hotelreservation;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class HotelReservationService {
     List<Hotels> hotelList = new ArrayList<>();
     Map<String, Integer> costOfHotels = new HashMap<>();
+    Map<String, String> monthsInYear = new HashMap<>();
 
     //Added hotel
     public void addHotelDetails(Hotels hotel) {
@@ -19,8 +23,56 @@ public class HotelReservationService {
         String dayEnd = dateCheckOut.substring(0, 2);
         int checkOutDay = Integer.parseInt(dayEnd);
         int numberOfDays = checkOutDay - checkInDay;
-        costOfHotels = hotelList.stream().collect(Collectors.toMap(Hotels::getHotelName, e -> e.getWeekdaysRates() * numberOfDays));
+        int numberOfWeekEnds = noOfWeekDaysAndWeekEnds(dateCheckIn, checkInDay, checkOutDay);
+        numberOfDays = numberOfDays - numberOfWeekEnds;
+        int finalNumberOfDays = numberOfDays;
+        costOfHotels = hotelList.stream().collect(Collectors.toMap(Hotels::getHotelName, e -> e.getWeekdaysRates() * finalNumberOfDays + e.getWeekendRates() * numberOfWeekEnds));
         return Collections.min(costOfHotels.values());
+    }
+    public int noOfWeekDaysAndWeekEnds(String dateCheckIn, int checkIn, int checkOut) {
+        int weekEndCount = 0;
+        String month = dateCheckIn.substring(2, 5);
+        int monthOfGivenDate = monthToNumber(month);
+        String year = dateCheckIn.substring(5, 9);
+        int yearOfGivenDate = Integer.parseInt(year);
+        for (int i = checkIn; i <= checkOut; i++) {
+            LocalDate localDate = LocalDate.of(yearOfGivenDate, monthOfGivenDate, i);
+            DayOfWeek dayOfWeek = DayOfWeek.from(localDate);
+            if (dayOfWeek.name().equals("SATURDAY") || dayOfWeek.name().equals("SUNDAY")) {
+                weekEndCount++;
+            }
+        }
+        return weekEndCount;
+    }
+
+    public int monthToNumber(String month) {
+        int monthToNumber = 0 ;
+        if (month.equals("Jan"))
+            monthToNumber = 1;
+        if (month.equals("Feb"))
+            monthToNumber = 2;
+        if (month.equals("Mar"))
+            monthToNumber = 3;
+        if (month.equals("Apr"))
+            monthToNumber = 4;
+        if (month.equals("May"))
+            monthToNumber = 5;
+        if (month.equals("Jun"))
+            monthToNumber = 6;
+        if (month.equals("Jul"))
+            monthToNumber = 7;
+        if (month.equals("Aug"))
+            monthToNumber = 8;
+        if (month.equals("Sep"))
+            monthToNumber = 9;
+        if (month.equals("Oct"))
+            monthToNumber = 10;
+        if (month.equals("Nov"))
+            monthToNumber = 11;
+        if (month.equals("Dec"))
+            monthToNumber = 12;
+
+        return monthToNumber;
     }
 
     public List<Hotels> getHotels() {
--- src/test/java/com/hotelreservation/HotelReservationServiceTest.java
+++ src/test/java/com/hotelreservation/HotelReservationServiceTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 public class HotelReservationServiceTest {
+    HotelReservationService hotelReservationService = new HotelReservationService();
+    Hotels Lakewood = new Hotels("Lakewood",110,90);
+    Hotels Bridgewood = new Hotels("Bridgewood",150,50);
+    Hotels RidgeWood = new Hotels("Ridgewood",220,150);
 
     @Test
     public void givenDate_shouldReturnLakeWood() {
-        HotelReservationService hotelReservationService = new HotelReservationService();
-        Hotels Lakewood = new Hotels("Lakewood",110,90);
-        Hotels Bridgewood = new Hotels("Bridgewood",150,50);
-        Hotels RidgeWood = new Hotels("Ridgewood",220,150);
         hotelReservationService.addHotelDetails(Lakewood);
         hotelReservationService.addHotelDetails(Bridgewood);
         hotelReservationService.addHotelDetails(RidgeWood);
@@ -22,14 +22,19 @@ public class HotelReservationServiceTest {
 
     @Test
     public void given3Hotels_shouldBeAbleToReturnCheapestHotel() {
-        HotelReservationService hotelReservationService = new HotelReservationService();
-        Hotels Lakewood = new Hotels("Lakewood",110,90);
-        Hotels Bridgewood = new Hotels("Bridgewood",150,50);
-        Hotels RidgeWood = new Hotels("Ridgewood",220,150);
+        hotelReservationService.addHotelDetails(Lakewood);
+        hotelReservationService.addHotelDetails(Bridgewood);
+        hotelReservationService.addHotelDetails(RidgeWood);
+        int cheapestHotel = hotelReservationService.findCheapestHotel("18May2021","21May2021");
+        Assertions.assertEquals(330,cheapestHotel);
+    }
+
+    @Test
+    void givenDateRangeWeShouldGetPriceAccordingToWeekDayAndWeekEnds() {
         hotelReservationService.addHotelDetails(Lakewood);
         hotelReservationService.addHotelDetails(Bridgewood);
         hotelReservationService.addHotelDetails(RidgeWood);
         int cheapestHotel = hotelReservationService.findCheapestHotel("20May2021","23May2021");
-        Assertions.assertEquals(330,cheapestHotel);
+        Assertions.assertEquals(250, cheapestHotel);
     }
 }
